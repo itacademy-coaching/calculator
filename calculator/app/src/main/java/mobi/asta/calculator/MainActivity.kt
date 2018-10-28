6fdbccb8ab75629ac06a02fd90dc43b04ad726b3
Andrii Stakhov <ranger.andrew@gmail.com>
2018-10-28 09:59:07 +0200

    first commit

---
+++ calculator/app/src/main/java/mobi/asta/calculator/MainActivity.kt
@@ -0,0 +1,27 @@
+package mobi.asta.calculator
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val btnClear = findViewById<Button>(R.id.button)
+//        val btnPersent = findViewById<Button>(R.id.button2)
+//        val btn9 = findViewById<Button>(R.id.button7)
+//
+//        btnClear.setOnClickListener {
+//            findViewById<TextView>(R.id.textView2).text = null
+//            findViewById<TextView>(R.id.textView).text = null
+//        }
+//
+//        btn9.setOnClickListener {
+//            findViewById<TextView>(R.id.textView2).text = "9"
+//        }
+    }
+}
